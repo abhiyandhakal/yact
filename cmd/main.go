8ad6ddb617ad9810be07c8c186cb8ee3e6623e7b
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2025-07-02 19:24:35 +0545

    feat: communication with socket in basic level

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	socketPath := "/tmp/yactd.sock"
+
+	addr, err := net.ResolveUnixAddr("unix", socketPath)
+	if err != nil {
+		fmt.Println("Error resolving address:", err)
+		return
+	}
+
+	conn, err := net.DialUnix("unix", nil, addr)
+	if err != nil {
+		fmt.Println("Error dialing:", err)
+		return
+	}
+	defer conn.Close()
+
+	fmt.Println("Connected to Unix socket:", socketPath)
+
+	// Write and read data over the connection
+	_, err = conn.Write([]byte("Hello from client!"))
+	if err != nil {
+		fmt.Println("Error writing:", err)
+		return
+	}
+
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading:", err)
+		return
+	}
+	fmt.Printf("Received: %s\n", string(buf[:n]))
 }
